36217513b529122aaa3c2ccbba96426073de7178
Elmar Athmer <elmar@athmer.org>
2019-09-18 03:12:06 +0200

    use macros to define services

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,29 @@
+#[macro_use]
+extern crate actix_web;
+
 use std::net;
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Result};
 
-fn get_index() -> impl Responder {
-    HttpResponse::Ok().body("GET - Hello world!")
+#[get("/")]
+fn get_index() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().body("GET - Hello world!"))
 }
 
-fn get_again() -> impl Responder {
-    HttpResponse::Ok().body("GET - Hello world again!")
+#[get("/again")]
+fn get_again() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().body("GET - Hello world again!"))
 }
 
-fn post_again() -> impl Responder {
-    HttpResponse::Ok().body("POST - Hello world again!")
+#[post("/again")]
+fn post_again() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().body("POST - Hello world again!"))
 }
 
 pub fn config_app(cfg: &mut web::ServiceConfig) {
-    cfg.route("/", web::get().to(get_index));
-    cfg.route("/again", web::get().to(get_again));
-    cfg.route("/again", web::post().to(post_again));
+    cfg.service(get_index);
+    cfg.service(get_again);
+    cfg.service(post_again);
 }
 
 pub fn run_server<A: net::ToSocketAddrs>(addr: A) -> std::io::Result<()> {
